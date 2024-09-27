09a5f51bf87d4783247a9896f1c9074d0ce37ccf
Antonio Galdes <antoniogaldes2@outlook.com>
2024-09-27 09:28:23 +0200

    Made some optimzation adjustments

--- lab1/lab1.cpp
+++ lab1/lab1.cpp
@@ -1,36 +1,59 @@
 #include "../shared/jbutil.h"
-#include <thread>
+#include <pthread.h>
 
 // Function to be integrated
 double f(double x) {
-   double  mu = 0.0;
-   double sigma = 1.0;
+   const double mu = 0.0;
+   const double sigma = 1.0;
 
-   return (1 / sqrt(2 * pi * sigma  * sigma)) * exp (-pow(x - mu, 2) / (2 * sigma * sigma));
+   return (1 / sqrt(2 * pi * sigma * sigma)) * exp(-pow(x - mu, 2) / (2 * sigma * sigma));
 }
 
+// Structure to pass arguments to the thread function
+struct ThreadArgs {
+    int N;
+    int* M;
+    double a;
+    double b;
+    double A;
+    double B;
+    jbutil::randgen* rng;
+};
+
 // Thread-safe Monte Carlo Function with loop unrolling
-void ThreadMC(const int N, int& M, const double a, const double b, const double A, const double B, jbutil::randgen& rng) {
+void* ThreadMC(void* args) {
+    ThreadArgs* threadArgs = static_cast<ThreadArgs*>(args);
+    int N = threadArgs->N;
+    int* M = threadArgs->M;
+    double a = threadArgs->a;
+    double b = threadArgs->b;
+    double A = threadArgs->A;
+    double B = threadArgs->B;
+    jbutil::randgen* rng = threadArgs->rng;
+
     int localM = 0;
-    for (int i = 0; i < N; i += 4) { // Unroll the loop by a factor of 4
-        double x1 = a + (b - a) * rng.fval();
-        double y1 = A + (B - A) * rng.fval();
+
+    // Unroll the loop by a factor of 4
+    for (int i = 0; i < N; i += 4) { 
+        double x1 = a + (b - a) * rng->fval();
+        double y1 = A + (B - A) * rng->fval();
         if (y1 < f(x1)) localM++;
 
-        double x2 = a + (b - a) * rng.fval();
-        double y2 = A + (B - A) * rng.fval();
+        double x2 = a + (b - a) * rng->fval();
+        double y2 = A + (B - A) * rng->fval();
         if (y2 < f(x2)) localM++;
 
-        double x3 = a + (b - a) * rng.fval();
-        double y3 = A + (B - A) * rng.fval();
+        double x3 = a + (b - a) * rng->fval();
+        double y3 = A + (B - A) * rng->fval();
         if (y3 < f(x3)) localM++;
 
-        double x4 = a + (b - a) * rng.fval();
-        double y4 = A + (B - A) * rng.fval();
+        double x4 = a + (b - a) * rng->fval();
+        double y4 = A + (B - A) * rng->fval();
         if (y4 < f(x4)) localM++;
     }
 
-    M += localM;
+    *M += localM;
+    return nullptr;
 }
 
 // Monte Carlo Method
@@ -47,32 +70,34 @@ void MonteCarlo(const int N) {
     const double A = 0.0;
     const double B = 0.5;
 
-    // Number of threads
-    const int nthreads = 4;
+    // Number of threads available by my personal computer
+    const int nthreads = 8;
 
-    // Dynamic arrays for threads, results and random numbers
-    jbutil::vector<std::thread> threads;
-    jbutil::vector<int> results(nthreads, 0);
-    jbutil::vector<jbutil::randgen> rng;
+    // Arrays for threads, results and random numbers
+    pthread_t threads[nthreads];
+    int results[nthreads] = {0};
+    jbutil::randgen rng[nthreads];
 
     // Initialize random number generators for each thread
     for (int i = 0; i < nthreads; ++i) {
-        rng.emplace_back(static_cast<unsigned long>(jbutil::gettime() * 1E6) + i);
+        rng[i] = jbutil::randgen(static_cast<unsigned long>(jbutil::gettime() * 1E6) + i);
     }
 
     // Divide the work among threads
     const int samplesPerThread = N / nthreads;
+    ThreadArgs threadArgs[nthreads];
     for (int i = 0; i < nthreads; ++i) {
-        threads.emplace_back(ThreadMC, samplesPerThread, std::ref(results[i]), a, b, A, B, std::ref(rng[i]));
+        threadArgs[i] = {samplesPerThread, &results[i], a, b, A, B, &rng[i]};
+        pthread_create(&threads[i], nullptr, ThreadMC, &threadArgs[i]);
     }
 
     // Wait for all threads to complete and combine results
     for (int i = 0; i < nthreads; ++i) {
-        threads[i].join();
+        pthread_join(threads[i], nullptr);
         M += results[i];
     }
 
-    double estimate = (static_cast<double>(M) / N) * (B-A) * (b-a) + A * (b-a);
+    double estimate = (static_cast<double>(M) / N) * (B - A) * (b - a) + A * (b - a);
     std::cerr << "Estimated integral: " << estimate << std::endl;
 
     t = jbutil::gettime() - t;
@@ -81,86 +106,12 @@ void MonteCarlo(const int N) {
     // Numerical result using error function
     double numericalResult = std::erf(sqrt(2));
     std::cerr << "\nNumerical result using error function: " << numericalResult << std::endl;
-
 }
 
-//Main program
+// Main program
 int main() {
 
     std::cerr << "Lab 1: Monte Carlo integral" << std::endl;
     const int N = int(1E8);
     MonteCarlo(N);
 }
-
-
-
-// #include <iostream>
-// #include <cmath>
-// #include <ctime>
-// #include "../shared/jbutil.h"
-
-
-// // Function to be integrated
-// double f(double x) {
-//    double  mu = 0.0;
-//    double sigma = 1.0;
-
-
-//    return (1 / sqrt(2 * pi * sigma  * sigma)) * exp (-pow(x - mu, 2) / (2 * sigma * sigma));
-// }
-
-
-// double randomInterval() {
-//    return (rand() + 1.0) / (RAND_MAX + 2.0);
-// }
-
-
-// // Monte Carlo Method
-// void MonteCarlo(const int N) {
-
-
-//    std::cerr << "\nImplementation (" << N << " samples)" << std::endl;
-
-
-//    // start timer
-//   double t = jbutil::gettime();
-
-
-//    int M = 0;
-//    double a = -2.0;
-//    double b = 2.0;
-//    double A = 0.0;
-//    double B = 0.5;
-
-
-//    for (int i = 0; i < N; ++i) {
-//        double xi = randomInterval() * (b-a) + a;
-//        double yi = randomInterval() * (B-A) + A;
-
-
-//        if (yi < f(xi)) {
-//            ++M;
-//        }
-//     }
-
-
-//     double estimate = (static_cast<double>(M) / N) * (B-A) * (b-a) + A * (b-a);
-//     std::cerr << "Estimated integral: " << estimate << std::endl;
-
-
-//   t = jbutil::gettime() - t;
-//   std::cerr << "Time taken: " << t << "s" << std::endl;
-
-
-// }
-
-
-// //Main program
-// int main() {
-
-
-//    std::cerr << "Lab 1: Monte Carlo integral" << std::endl;
-//    const int N = int(1E8);
-//    MonteCarlo(N);
-// }
-
